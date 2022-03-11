7190f4142534714727c8b823491d41e5eacda11a
olw0905 <oul@anl.gov>
2022-03-11 12:13:58 -0600

    Improved calc.py. Can work with any sheetname now. Added jupyter notebooks for debugging.

--- calc.py
+++ calc.py
@@ -1,23 +1,22 @@
 import pandas as pd
 # from lookup_table import lookup_table
-from utils import unit_conversion, convert_transport_lci, step_processing, used_other_process, process, format_input, calculate_lca, lookup_table
+from utils import process, format_input, calculate_lca
 
 category = pd.read_csv("category.csv", index_col=0, header=0).squeeze()
 
 def calc(lci_file):
-    lci_feedstock = pd.read_excel(lci_file, sheet_name='Feedstock harvest')
-    lci_feedstock = format_input(lci_feedstock)
+    xl = pd.ExcelFile(lci_file)
+    sheet_names = xl.sheet_names
 
-    lci_preprocessing = pd.read_excel(lci_file, sheet_name='Preprocessing')
-    lci_preprocessing = format_input(lci_preprocessing)
+    step_mapping = dict()
+    for sheet in sheet_names:
+        df = pd.read_excel(lci_file, sheet_name=sheet)
+        df = format_input(df)
+        step_mapping.update({sheet.lower(): df})
 
-    lci_fuel = pd.read_excel(lci_file, sheet_name='Fuel Production')
-    lci_fuel = format_input(lci_fuel)
-
-    step_mapping = {'feedstock harvest': lci_feedstock, 'preprocessing': lci_preprocessing, 'fuel production': lci_fuel}
     res = process(step_mapping)
 
-    overall_lci = res['fuel production']
+    overall_lci = res[sheet_names[-1].lower()]    # Assuming the final product is always in the last worksheet TODO: Improve this.
     overall_lci = overall_lci[overall_lci['Type']=='Input'].copy()
     # overall_lci['ID'] = overall_lci.apply(
     #     # lambda a: a['Resource'] if (pd.isna(a['End Use']))|(a['Resource'] == 'Electricity') else a['Resource']+'_'+a['End Use'], axis=1
