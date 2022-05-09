fbadb4b6c69310cc03d67498faf4de92f9b860bd
oulongwen <oulongwen@gmail.com>
2022-05-08 22:43:34 -0500

    Add a function for sensitivity analysis

--- app.py
+++ app.py
@@ -448,7 +448,7 @@ def update_results(
     dropdown_items = []
     lci_data = {}
     data_status = "OK"
-    error_message = ""
+    # error_message = ""
 
     ctx = dash.callback_context
     changed_id = ctx.triggered[0]["prop_id"].split(".")[0]
@@ -533,7 +533,7 @@ def update_results(
             #     html.Br(),
             #     data_status,
             # ]
-            error_message = data_status
+            # error_message = data_status
     # elif 'update-lci' in changed_id:
     #     # step_mapping = {sheet.lower(): format_input(df) for sheet, df in lci_mapping.items()}
     #     # res_new = calc(sheet_names, step_mapping)
@@ -664,18 +664,37 @@ def update_figures(json_data, tab, re, rs, us, es, em, f1, f2, f3, f4):
     # else:
     #     return f1, f2, f3, f4, reset_status, update_status, error_status, error_message
 
+
 def sensitivity_analysis(list_of_contents, list_of_names, list_of_dates):
     """
     Calcualte LCA results for multiple LCI files
     """
     if list_of_contents is not None:
-        resutls = {}
-        for content, filename, date in zip(list_of_contents, list_of_names, list_of_dates):
+        df = pd.DataFrame()
+        sensitivity_error_status = False
+        sensitivity_error_message = [
+            html.H5(
+                "Errors are found in the following LCI files: ",
+                className="alert-heading",
+            )
+        ]
+
+        for content, filename, date in zip(
+            list_of_contents, list_of_names, list_of_dates
+        ):
             lci_file = parse_contents(content, filename, date)
             res = calculation_in_one(lci_file)
-            resulsts.update(filename: res)
+            if isinstance(res, str):
+                if not sensitivity_error_status:
+                    sensitivity_error_status = True
+                sensitivity_error_message.extend(
+                    [html.Br(), html.H6(lci_file + ": " + res)]
+                )
+            else:
+                res["FileName"] = lci_file
+                df = pd.concat([df, res], ignore_index=True)
 
-        return results
+        return df, sensitivity_error_status, sensitivity_error_message
 
 
 # @app.callback(
